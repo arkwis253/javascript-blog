667c636340ddd009facbf65cd7801f60231abedc
Arkadiusz Wiśniewski <darek253633@gmail.com>
2023-08-02 00:08:13 +0200

    create .editorconfig file

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ function titleClickHandler(event){
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
-  // remove class 'active' from all article links 
+  // remove class 'active' from all article links
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  // add class 'active' to the clicked link 
+  // add class 'active' to the clicked link
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
   // remove class 'active' from all articles
@@ -19,13 +19,13 @@ function titleClickHandler(event){
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  // get 'href' attribute from the clicked link 
+  // get 'href' attribute from the clicked link
   const articleSelector = clickedElement.getAttribute('href');
   // find the correct article using the selector (value of 'href' attribute)
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
   // add class 'active' to the correct article
-  targetArticle.classList.add('active'); 
+  targetArticle.classList.add('active');
 }
 
 //generate sidebar links
